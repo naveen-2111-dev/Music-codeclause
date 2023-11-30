43aefb14680ff5cfa8922c5dd81881f9028438db
Naveen <sce22cs026@sairamtap.edu.in>
2023-11-30 19:49:45 +0530

    init

--- music.js
+++ music.js
@@ -1,9 +1,32 @@
-var player = document.getElementById('play');
+const audio = document.getElementById("play");
+const playPauseIcon = document.getElementById("right");
+const rangeInput = document.querySelector('input[type="range"]');
 
 function PlayControler() {
-    if (player.paused) {
-        player.play();
-    } else {
-        player.pause();
-    }
+  if (audio.paused) {
+    audio.play();
+    playPauseIcon.classList.remove("fa-caret-right");
+    playPauseIcon.classList.add("fa-pause");
+  } else {
+    audio.pause();
+    playPauseIcon.classList.remove("fa-pause");
+    playPauseIcon.classList.add("fa-caret-right");
+  }
 }
+
+audio.addEventListener("timeupdate", function () {
+  const percentage = (audio.currentTime / audio.duration) * 100;
+  rangeInput.value = percentage;
+});
+
+rangeInput.addEventListener("input", function () {
+  const getTime = (rangeInput.value / 100) * audio.duration;
+  audio.currentTime = getTime;
+});
+
+audio.addEventListener("ended", function () {
+  playPauseIcon.classList.remove("fa-pause");
+  playPauseIcon.classList.add("fa-caret-right");
+  rangeInput.value = 0;
+});//when song completes the icon will automatically change to play icon....
+
